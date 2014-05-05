0e0cbb733e43df7169c16dd663b6b966c86baccb
Tauqeer Ahmad <tauqeer.ahmad@square63.com>
2014-05-05 11:48:20 +0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieRecommendation::Application.config.secret_token = '6e2a29f16b52e0a679d537b52ab2b591182625b7d326bdbd8410b840eb6afafbccc995235c32997c76b2c88978314cffce5cb655c4d1be1c3663d82942c67869'
